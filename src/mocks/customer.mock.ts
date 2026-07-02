81384060db47fda7dd249ab05387c48681f6ad1c
Ricardo Linhares <ricardofilho887@gmail.com>
2026-07-02 16:42:43 -0300

    refactor: move mocked data to a dedicated directory

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from "../types.ts";
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: "Lionel Messi",
+		email: "leomessi@email.com",
+		status: true,
+	},
+	{
+		id: 2,
+		name: "Cristiano Ronaldo",
+		email: "robozao@email.com",
+		status: true,
+	},
+	{
+		id: 2,
+		name: "Neymar Jr",
+		email: "neymarjr@email.com",
+		status: false,
+	},
+];
