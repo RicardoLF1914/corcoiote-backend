94bdcdcc90318928c665b9501256b6c89807bfcc
Ricardo Linhares <ricardofilho887@gmail.com>
2026-07-14 20:19:07 -0300

    feat: connect invoice router to server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import CustomerRouter from "./routes/customer.router.ts";
+import InvoiceRouter from "./routes/invoice.router.ts";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/customers", CustomerRouter);
+app.use("/invoices", InvoiceRouter);
 
 app.use((_request, response) => {
 	response.status(404).json({ message: "NOT FOUND" });
