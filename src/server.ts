bdee27b7ac53f2f382b438236ecd85844af3ef9a
Ricardo Linhares <ricardofilho887@gmail.com>
2026-07-16 16:52:57 -0300

    feat: connect middleware for request logging and error handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import errorHandler from "./middlewares/errorHandler.ts";
+import requestLogger from "./middlewares/requestLogger.ts";
 import CustomerRouter from "./routes/customer.router.ts";
 import InvoiceRouter from "./routes/invoice.router.ts";
 
 const app = express();
+
+app.use(requestLogger);
 
 app.use(express.json());
 
@@ -10,7 +14,9 @@ app.use("/customers", CustomerRouter);
 app.use("/invoices", InvoiceRouter);
 
 app.use((_request, response) => {
-	response.status(404).json({ message: "NOT FOUND" });
+	response.status(404).json({ message: "Página não encontrada" });
 });
+
+app.use(errorHandler);
 
 app.listen(Number(process.env.PORT));
