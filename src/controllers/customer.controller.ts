6f580abc7d45d78ad1352de73d12cb0691c56870
Ricardo Linhares <ricardofilho887@gmail.com>
2026-07-23 17:00:17 -0300

    feat: add error validation for bad requests

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,8 +1,11 @@
 import type { Request, Response } from "express";
+import type {
+	CreateCustomerInput,
+	UpdateCustomerInput,
+} from "../schemas/customer.schema.ts";
 import * as CustomerService from "../services/customer.service.ts";
-import type { CreateCustomer, UpdateCustomer } from "../types.ts";
 
-export function getAllCustomer(_request: Request, response: Response) {
+export function getAllCustomers(_request: Request, response: Response) {
 	const customers = CustomerService.findAllCustomers();
 
 	response.status(200).json(customers);
@@ -17,7 +20,7 @@ export function getCustomerById(request: Request, response: Response) {
 }
 
 export function createCustomer(request: Request, response: Response) {
-	const { name, email } = request.body as CreateCustomer;
+	const { name, email } = request.body as CreateCustomerInput;
 
 	const customer = CustomerService.insertCustomer({ name, email });
 
@@ -26,10 +29,13 @@ export function createCustomer(request: Request, response: Response) {
 
 export function updateCustomer(request: Request, response: Response) {
 	const id = Number(request.params.id);
+	const { name, email, status } = request.body as UpdateCustomerInput;
 
-	const { name, email, status } = request.body as UpdateCustomer;
-
-	const customer = CustomerService.modifyCustomer(id, { name, email, status });
+	const customer = CustomerService.modifyCustomer(id, {
+		name,
+		email,
+		status,
+	});
 
 	response.status(200).json(customer);
 }
