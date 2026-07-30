c9babe93eff6dabbeb64f11de94f45a9aeb05119
Ricardo Linhares <ricardofilho887@gmail.com>
2026-07-30 16:31:45 -0300

    refactor: update request logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import PinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
 import logger from "./lib/logger.ts";
 import errorHandler from "./middlewares/errorHandler.ts";
 import CustomerRouter from "./routes/customer.router.ts";
 
 const app = express();
 
-app.use();
+app.use(pinoHttp({ logger }));
 
 app.use(express.json());
 
