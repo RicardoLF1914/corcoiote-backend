0e8f3ffac1b7a97e82ce6abcd85fe44f350fd37a
Ricardo Linhares <ricardofilho887@gmail.com>
2026-09-02 00:52:17 +0000

    refactor: implement object relational mapper

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,6 +1,5 @@
-import { Prisma } from "@prisma/client/extension";
 import { NotFoundError } from "../errors/index.ts";
-import { prisma } from "../lib/prisma.ts";
+import prisma from "../lib/prisma.ts";
 import { customers } from "../mocks/customer.mock.ts";
 import type {
 	CreateCustomer,
@@ -8,51 +7,64 @@ import type {
 } from "../schemas/customer.schema.ts";
 import type { Customer } from "../types.ts";
 
-export function findAllCustomers(): Customer[] {
+export async function findAllCustomers(): Promise<Customer[]> {
+	const customers = await prisma.customer.findMany();
+
 	return customers;
 }
 
-export function findCustomerById(id: number): Customer {
-	const customer = customers.find((c) => c.id === id);
+export async function findCustomerById(id: number): Promise<Customer> {
+	const customer = await prisma.customer.findUnique({ where: { id } });
 
 	if (!customer) throw new NotFoundError(`Cliente de id ${id} não encontrado`);
 
 	return customer;
 }
 
-export function insertCustomer({ name, email }: CreateCustomer): Customer {
-	const customer: Customer = {
-		id: customers[customers.length - 1].id + 1,
-		name: name,
-		email: email,
-		status: true,
-	};
-
-	customers.push(customer);
+export async function insertCustomer({
+	name,
+	email,
+	imageUrl,
+}: CreateCustomer): Promise<Customer> {
+	const customer = await prisma.customer.create({
+		data: {
+			name,
+			email,
+			imageUrl,
+		},
+	});
 
 	return customer;
 }
 
-export function modifyCustomer(
+export async function modifyCustomer(
 	id: number,
-	{ name, email, status }: UpdateCustomer,
-): Customer {
-	const customer = customers.find((c) => c.id === id);
+	{ name, email, imageUrl }: UpdateCustomer,
+): Promise<Customer> {
+	const findCustomer = await prisma.customer.findUnique({ where: { id } });
 
-	if (!customer) throw new NotFoundError(`Cliente de id ${id} não encontrado`);
+	if (!findCustomer) {
+		throw new NotFoundError("Cliente não encontrado.");
+	}
 
-	if (name) customer.name = name;
-	if (email) customer.email = email;
-	if (status !== undefined) customer.status = status;
+	const customer = await prisma.customer.update({
+		where: { id },
+		data: {
+			name,
+			email,
+			imageUrl,
+		},
+	});
 
 	return customer;
 }
 
-export function removeCustomer(id: number): void {
-	const index = customers.findIndex((c) => c.id === id);
+export async function removeCustomer(id: number): Promise<void> {
+	const findCustomer = await prisma.customer.findUnique({ where: { id } });
 
-	if (index === -1)
-		throw new NotFoundError(`Cliente de id ${id} não encontrado`);
+	if (!findCustomer) {
+		throw new NotFoundError("Cliente não encontrado.");
+	}
 
-	customers.splice(index, 1);
+	await prisma.customer.delete({ where: { id } });
 }
